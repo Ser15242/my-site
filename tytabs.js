5585bad0ec7454375e87164087a73832ebcbcaea
Ser15242 <108604834+Ser15242@users.noreply.github.com>
2022-07-03 09:53:19 +0300

    Add files via upload

---
+++ tytabs.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$("#tabsholder").tytabs({
+							tabinit:"1",
+							fadespeed:"fast"
+							});
+	$("#tabsholder2").tytabs({
+							prefixtabs:"tabz",
+							prefixcontent:"contentz",
+							classcontent:"tabscontent",
+							tabinit:"1",
+							catchget:"tab2",
+							fadespeed:"normal"
+							});
+$("#tabsholder3").tytabs({
+							prefixtabs:"tabo",
+							prefixcontent:"contento",
+							classcontent:"tabscontent",
+							tabinit:"1",
+							catchget:"tab2",
+							fadespeed:"normal"
+							});							
+});
+
+
